81fc9406f7b40da44d7484b727091321724f8a48
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-12-26 20:38:56 +0200

    Factory pattern.

--- src/buttons/HtmlButton.java
+++ src/buttons/HtmlButton.java
@@ -1,4 +1,13 @@
 package buttons;
 
-public class HtmlButton {
+public class HtmlButton implements Button {
+
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+
+    public void onClick() {
+        System.out.println("Click! Button says - 'Hello World!'");
+    }
 }
